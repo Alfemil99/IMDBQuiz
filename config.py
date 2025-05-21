1cc3029f23927748ca92e2e0427c58e9c8fcfbc2
Alf Emil <Alfemil99@gmail.com>
2025-05-21 15:44:20 +0200

    Initial commit: Flask + PostgreSQL app

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+DB_NAME = "myappdb"
+DB_USER = "myuser"
+DB_PASSWORD = "mypassword"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+SQLALCHEMY_DATABASE_URI = "postgresql://myuser:mypassword@localhost:5432/myappdb"
+
